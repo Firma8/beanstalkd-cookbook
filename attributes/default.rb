ffced778fe5533b36ba3c2e642c33a619c5c33f2
Josh Blancett <joshblancett@gmail.com>
2014-06-24 16:31:26 -0400

    a basic start

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+
+default['beanstalkd']['listen_addr'] = '127.0.0.1'
+default['beanstalkd']['listen_port'] = '11300'
+
+default['beanstalkd']['persistent_storage'] = false
+default['beanstalkd']['extra_opts'] = ""
+
+default['beanstalkd']['enable_startup'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,5 +4,26 @@
 #
 # Copyright 2014, Firma 8
 #
-# All rights reserved - Do Not Redistribute
-#
+
+package 'beanstalkd' do
+  action :upgrade
+end
+
+template '/etc/default/beanstalkd' do
+  source 'beanstalkd.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  variables beanstalkd: node['beanstalkd']
+  notifies :restart, 'service[beanstalkd]'
+end
+
+svc = service 'beanstalkd' do
+  start_command '/etc/init.d/beanstalkd start'
+  stop_command '/etc/init.d/beanstalkd stop'
+  status_command '/etc/init.d/beanstalkd status'
+  supports [:start, :stop, :status]
+  action :nothing
+end
+
+svc.run_action(:enable) if node['beanstalkd']['enable_startup']
