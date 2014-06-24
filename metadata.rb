3b8f115f5e886013aa1ca87d293ca328b664d746
Josh Blancett <joshblancett@gmail.com>
2014-06-24 16:11:25 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'beanstalkd'
+maintainer       'Firma 8'
+maintainer_email 'josh@firma8.com'
+license          'All rights reserved'
+description      'Installs/Configures beanstalkd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: beanstalkd
+# Recipe:: default
+#
+# Copyright 2014, Firma 8
+#
+# All rights reserved - Do Not Redistribute
+#
